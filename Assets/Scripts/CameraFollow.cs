4be5f2eec6501d15a0c9b03051e150f3c4d4365d
thanktoanf <thanhtoan0306@gmail.com>
2025-03-28 22:45:13 +0700

    camera-follow1

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    Vector3 offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
