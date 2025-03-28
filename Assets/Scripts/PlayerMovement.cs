dc2cd3ff46192c528328b4784c207f68924106ec
thanktoanf <thanhtoan0306@gmail.com>
2025-03-28 22:29:06 +0700

    moving left right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5; // Speed of the player
     public Rigidbody rb; // Reference to the Rigidbody2D component
 
+    float horizontalInput;
+
     private void FixedUpdate () 
     {
         Vector3 forwardMove = transform.forward * speed * Time.deltaTime; // Calculate forward movement
-        rb.MovePosition(rb.position + forwardMove); // Move the player forward
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.deltaTime; // Calculate horizontal movement
+        rb.MovePosition(rb.position + forwardMove + horizontalMove); // Move the player forward
 
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,6 +23,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
